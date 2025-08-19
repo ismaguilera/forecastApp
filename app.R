51c531e5259a60c147fefb6728bcd7240fee6612
ismaguilera <iaguiler@ug.uchile.cl>
2025-08-19 10:17:21 -0400

    Fix: Actualizar la configuración de la aplicación y restaurar el archivo de configuración de forecastApp.dcf

--- app.R
+++ app.R
@@ -5,6 +5,8 @@
 # This file is the entry point for shinyapps.io
 # It ensures the package is loaded before running the app.
 
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+
 # Set options for Golem app in production
 options("golem.app.prod" = TRUE)
 
@@ -16,4 +18,8 @@ rm(list = ls(all.names = TRUE))
 golem::document_and_reload()
 
 # Run the application
-run_app()
+# run_app()
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
+forecastApp::run_app() # Reemplaza "forecastApp" con el nombre de tu paquete si es diferente
